c84e04ee7a6f0d0fa03ec09c6db70bd95887715a
AutomationTest001 <automationtest307@gmail.com>
2022-06-01 19:43:07 +0530

    apr batch test cases for cucucmber

---
+++ src/main/java/com/selenium/cucumber/selenium_cucumbers/App.java
@@ -0,0 +1,13 @@
+package com.selenium.cucumber.selenium_cucumbers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
